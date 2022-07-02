37492a469beba796de4431b4d7fb5e849543621a
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-02 12:14:53 +0100

    init

--- components/AddButton.js
+++ components/AddButton.js
@@ -1,40 +1,89 @@
 /* eslint-disable */
-import {View, Text, TouchableOpacity, Modal} from 'react-native';
+import {View, Text, TouchableOpacity, Modal ,StyleSheet ,TextInput} from 'react-native';
 import React, {useState} from 'react';
 import TakeTodo from './TakeTodo';
 
-import Icon from 'react-native-vector-icons/AntDesign';
+import Icon from 'react-native-vector-icons//MaterialIcons';
 
 const AddButton = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [text, setText] = useState('')
+
   return (
     <View>
       <Modal visible={modalVisible} animationType="slide" transparent={true}>
         <View
           style={{
             borderRadius:20,
-            height: '40%',
+            height: '60%',
             marginTop: 'auto',
             backgroundColor: '#f5f5f5',
             elevation:5
           }}>
-          <Icon
-            name="close"
-            size={20}
-            color={'#30B0D9'}
-            onPress={() => setModalVisible(false)}
-          />
+         
 
-          <TakeTodo />
+          <View style={styles.container}>
+     <View>
+      <TextInput
+        onChange={(val) => setText(val)}
+        value={text}
+        placeholder='Add Todo'
+        style={styles.input}
+        
+        
+      />
+      </View>
+     
+      
+    </View>
+
+          <View style={styles.iconContainer}>
+
+<TouchableOpacity  >
+<Icon name='cancel' color={'red'}size={50} onPress={()=> setModalVisible(false)} />
+</TouchableOpacity>
+
+<TouchableOpacity  >
+<Icon name='check-circle' color={'#09f700'}size={50}  />
+</TouchableOpacity>
+
+<TouchableOpacity  >
+<Icon name='image' color={'#30B0D9'}size={50}  />
+</TouchableOpacity>
+
+</View>
         </View>
       </Modal>
 
       <TouchableOpacity onPress={() =>setModalVisible(true)}>
-        <Icon name="pluscircle" size={50} color={'#30B0D9'} />
+        <Icon name="add-circle" size={50} color={'#30B0D9'} />
         </TouchableOpacity>
     
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  iconContainer:{
+
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+    marginTop:10
+ },
+ container: {
+  width: '100%',
+  marginTop: 10,
+},
+input: {
+  backgroundColor: '#F2F5FF',
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  borderWidth: 1,
+  borderRadius: 5,
+  borderColor: '#e8e8e8',
+  marginLeft: 10,
+  marginRight: 10,
+},
+
+})
 export default AddButton;
--- components/TakeTodo.js
+++ components/TakeTodo.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 import { View, Text } from 'react-native'
 import React from 'react'
-import TakeInput from './TakeInput'
+
 
 const TakeTodo = () => {
   return (
     <View>
-      <TakeInput/>
+     <Text>hi</Text>
     </View>
   )
 }
