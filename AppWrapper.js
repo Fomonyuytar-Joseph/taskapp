03d542a6ead69987b991ecfa3c1ab4fbcdb83db5
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-08 15:52:18 +0100

    init

--- AppWrapper.js
+++ AppWrapper.js
@@ -1,9 +1,13 @@
 /* eslint-disable */
-
+import 'react-native-gesture-handler';
 import * as React from 'react'
 import App from './App'
 import store from './redux/store'
 import { Provider } from 'react-redux'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// const Stack = createStackNavigator();
 
 
 
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 /* eslint-disable */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+  
+    'react-native-reanimated/plugin',
+],
  
 };
